9ae03e956a91af8cd42589bdd829ef5ac9418b72
SoPhors <91106971+PhoSophors@users.noreply.github.com>
2023-01-16 17:12:39 +0700

    top picture using with HTMk

--- src/Views/Top-Picture.jsx
+++ src/Views/Top-Picture.jsx
@@ -8,7 +8,18 @@ function Picture() {
     return (
         <>
         <div>
-            top pictiure
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Document</title>
+            </head>
+            <body>
+                <h1>Top Picture using</h1>
+                
+            </body>
+            </html>
         </div>
         </>
     );
